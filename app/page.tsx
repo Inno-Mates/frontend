229b17baad3027a4f5da259353ab669b27f2bd61
vedang2106 <vedangkolapate745@gmail.com>
2025-09-16 12:34:36 +0530

    rebase

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <>
       <ClientHeroSection />
       <HowItWorksSection />
-      <ImpactSection />
+      {/* <ImpactSection /> */}
       <IssueCategoriesSection />
       <CommunitySection />
       <MobileAppSection />
